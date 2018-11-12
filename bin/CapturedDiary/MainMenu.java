bb97a16fae8d37ae40ec48eadc5b4c8156141c00
岡嶋隆人 <nnkrbyok_hurnkznrikg@outlook.jp>
2018-11-12 13:18:05 +0900

    MainMenu(kari)

--- bin/CapturedDiary/MainMenu.java
+++ bin/CapturedDiary/MainMenu.java
@@ -5,12 +5,12 @@ import java.awt.*;
 import java.awt.event.*;
 
 class Preframe extends Frame{
-	
-	
+
+
 	public PreFrame(String title) {
-		set Title(title);
-		
-		
+		setTitle(title);
+
+
 		addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				System.exit(0);
@@ -20,22 +20,22 @@ class Preframe extends Frame{
 }
 
 public class MainMenu {
-	
-	
+
+
 	public static void main(String args[]) {
 		PreFrame frm = new PreFrame("釣られ日誌");
 		frm.setLocation(640.480);
 		frm.setSize(640,480);
 		frm.setBackground(Color.LIGHT_GRAY);
 		frm.setVisible(true);
-		
+
 		Button start = new Button();
 		start.setLabel("ゲーム開始");
 		frm.add(start);
-		
+
 		Button lib = new Button();
 		lib.setLabel("図鑑");
 		frm.add(lib);
-		
+
 	}
 }
