7e04f49f9e03f28f30561ad859b9f532a7cfba63
岡嶋隆人 <nnkrbyok_hurnkznrikg@outlook.jp>
2018-11-15 12:32:19 +0900

    MainMenu 30%

--- bin/src/CapturedDiary/MainMenu.java
+++ bin/src/CapturedDiary/MainMenu.java
@@ -1,10 +1,36 @@
 package CapturedDiary;
 
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class MainMenu {
 
 	public static void main(String args[]) {
 		MainWindow m = new MainWindow();
 		m.setVisible(true);
+	}
+}
+
+class MainWindow extends Frame {
+	public MainWindow() {
+	    setSize(640,480);
+	    setLayout(new FlowLayout());
+        Button start = new Button();
+        start.setLabel("ゲーム開始");
+	    add(start);
+     	Button lib = new Button();
+	    lib.setLabel("図鑑");
+	    add(lib);
+	    addWindowListener(new MainExit());
+	}
+}
+
+class MainExit extends WindowAdapter
+{
+	public void windowClosing(WindowEvent e) {
+		System.exit(0);;
 	}
 }
