3e38d4140130e7204510b15e734c1f1c0cdbad3b
岡嶋隆人 <nnkrbyok_hurnkznrikg@outlook.jp>
2018-11-19 12:29:26 +0900

    MainMenu80%

--- bin/CapturedDiary/MainMenu.java
+++ bin/CapturedDiary/MainMenu.java
@@ -1,39 +1,72 @@
-package CapturedDiary;
+package jp.nyatla.nyartoolkit.jogl.sample.artk.sketch.webcamcapture;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.Button;
+import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
-class PrefFrame extends Frame{
+public class MainMenu
+{
 
+	public static void main(String[] args) {
+		MainWindow mw = new MainWindow("釣られ日誌");//ウィンドウタイトルを釣られ日誌に設定しオブジェクトMainWindowの呼び出し
+		mw.setVisible(true);//ウィンドウ(メインメニュー)の表示
+		return;
+	}
+}
 
-	public PrefPreFrame(String title) {
+class MainWindow extends Frame
+{
+	public MainWindow(String title)//文字列型タイトルを受け取り、表示するウィンドの設定
+	{
 		setTitle(title);
+	    setSize(640,480);
+	    setLayout(null);
 
+        Button start = new Button();//ゲームスタートボタンのセッティング
+        start.setLabel("ゲーム開始");
+        start.setSize(100,40);
+        start.setLocation(270,150);
+	    add(start);
+	    start.addActionListener(new StartPush());
 
-		addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent e) {
-				System.exit(0);
-			}
-		});
+     	Button lib = new Button();//図鑑表示ボタンのセッティング
+	    lib.setLabel("図鑑");
+	    lib.setSize(100,40);
+	    lib.setLocation(270,320);
+	    add(lib);
+	    lib.addActionListener(new LibPush());
+
+	    addWindowListener(new MainExit());
 	}
 }
 
-public class MainMenu {
-
-
-	public static void main(String args[]) {
-		PreFrame frm = new PreFrame("釣られ日誌");
-		frm.setSize(640,480);
-		frm.setBackground(Color.LIGHT_GRAY);
-		frm.setVisible(true);
-
-		Button start = new Button();
-		start.setLabel("ゲーム開始");
-		frm.add(start);
-
-		Button lib = new Button();
-		lib.setLabel("図鑑");
-		frm.add(lib);
-
+class MainExit extends WindowAdapter
+{
+	public void windowClosing(WindowEvent e)
+	{
+		System.exit(0);
 	}
 }
+
+class StartPush implements ActionListener
+{
+	private static final String[] args = null;
+
+	public void actionPerformed(ActionEvent e)
+	{
+		WebCamSample.main(args);
+	}
+}
+
+class LibPush implements ActionListener
+{
+   private static final String[] args = null;
+   public void actionPerformed(ActionEvent e)
+   {
+
+   }
+}
+
--- bin/src/CapturedDiary/MainMenu.java
+++ bin/src/CapturedDiary/MainMenu.java
@@ -10,28 +10,29 @@ import java.awt.event.WindowEvent;
 public class MainMenu
 {
 
-	public static void main(String args[]) {
-		MainWindow mw = new MainWindow("釣られ日誌");
-		mw.setVisible(true);
+	public static void main(String[] args) {
+		MainWindow mw = new MainWindow("釣られ日誌");//ウィンドウタイトルを釣られ日誌に設定しオブジェクトMainWindowの呼び出し
+		mw.setVisible(true);//ウィンドウ(メインメニュー)の表示
+		return;
 	}
 }
 
 class MainWindow extends Frame
 {
-	public MainWindow(String title)
+	public MainWindow(String title)//文字列型タイトルを受け取り、表示するウィンドの設定
 	{
 		setTitle(title);
 	    setSize(640,480);
 	    setLayout(null);
 
-        Button start = new Button();
+        Button start = new Button();//ゲームスタートボタンのセッティング
         start.setLabel("ゲーム開始");
         start.setSize(100,40);
         start.setLocation(270,150);
 	    add(start);
 	    start.addActionListener(new StartPush());
 
-     	Button lib = new Button();
+     	Button lib = new Button();//図鑑表示ボタンのセッティング
 	    lib.setLabel("図鑑");
 	    lib.setSize(100,40);
 	    lib.setLocation(270,320);
@@ -46,20 +47,23 @@ class MainExit extends WindowAdapter
 {
 	public void windowClosing(WindowEvent e)
 	{
-		System.exit(0);;
+		System.exit(0);
 	}
 }
 
 class StartPush implements ActionListener
 {
+	private static final String[] args = null;
+
 	public void actionPerformed(ActionEvent e)
 	{
-
+		WebCamSample.main(args);
 	}
 }
 
 class LibPush implements ActionListener
 {
+   private static final String[] args = null;
    public void actionPerformed(ActionEvent e)
    {
 
