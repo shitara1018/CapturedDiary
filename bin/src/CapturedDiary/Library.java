f62351bc67f23b189665f848d1dcde2b1e3a4914
岡嶋隆人 <nnkrbyok_hurnkznrikg@outlook.jp>
2018-11-19 19:05:18 +0900

    Library 20%

--- bin/src/CapturedDiary/Library.java
+++ bin/src/CapturedDiary/Library.java
@@ -6,12 +6,11 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class Library {
-
 	
+			
 	public static void main(String[] args) {
 		LibraryWindow lw = new LibraryWindow("図鑑");
-		lw.setVisible(true);
-		return;
+	    lw.setVisible(true);
 	}
 }
 
@@ -21,23 +20,22 @@ class LibraryWindow extends Frame
 	{
 		int sws_x = 1280;
 		int sws_y = 980;
-		
+		int bts_x = 150;
+	    int bts_y = 150;
 		
 		setTitle(title);
 		setSize(sws_x, sws_y);
 		setLayout(null);
 		
-	    int bts_x = 150;
-	    int bts_y = 150;
 		
-		for(int i = 1; i <= 4; i++) 
+		for(int i = 0; i < 4; i++) 
 		{
-			for(int j = 1; j <= 4; j++) 
+			for(int j = 0; j < 4; j++) 
 			{
 				Button fish = new Button();
 				fish.setLabel("魚");
 				fish.setSize(bts_x,bts_y);
-				fish.setLocation((sws_x / 5) * j - (bts_x / 2), (sws_y / 5) * i - ( bts_y / 2) );
+				fish.setLocation((sws_x / 5) * (j + 1) - (bts_x / 2), (sws_y / 5) * (i + 1) - ( bts_y / 2) );
 				add(fish);
 			}
 		}
@@ -50,6 +48,7 @@ class LibraryExit extends WindowAdapter
 {
 	public void windowClosing(WindowEvent e) 
 	{
-		System.exit(0);
+		e.getWindow().dispose();
 	}
+
 }
